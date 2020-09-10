6022d20c3f8c2d81b04cf477685d56cf142bbbfc
tracer-ics2020 <tracer@njuics.org>
2020-09-10 15:22:27 +0800

    >  compile 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: 8085ce11e1a24fff80dfa3cdaff7dc02   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-47-generic       Architecture: x86-64  15:22:27 up 17:34,  1 user,  load average: 1.31, 1.39, 1.33

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -48,6 +48,7 @@ static int cmd_q(char *args) {
 }
 
 static int cmd_si(char *args){
+  printf("srgs:::\n");
    if(is_empty_arg(args)){
      cpu_exec(1);
      return 0;
@@ -133,6 +134,7 @@ void ui_mainloop() {
     int i;
     for (i = 0; i < NR_CMD; i ++) {
       if (strcmp(cmd, cmd_table[i].name) == 0) {
+        printf("equal");
         if (cmd_table[i].handler(args) < 0) { return; }
         break;
       }
