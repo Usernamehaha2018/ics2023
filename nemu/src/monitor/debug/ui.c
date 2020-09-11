f9b48087a9cb29a581ca04819bf25dff2250d01e
tracer-ics2020 <tracer@njuics.org>
2020-09-11 20:10:15 +0800

    >  compile 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: d806712f879e4edfbbe713b1e8c25455   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-47-generic       Architecture: x86-64  20:10:15 up 1 day, 6 min,  1 user,  load average: 1.02, 0.97, 0.75

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -112,7 +112,7 @@ static int cmd_x(char *args){
 }
 
 static int cmd_p(char *args){
-  char *e = "700 + 12 -2 + *($ebp)*199 ";
+  char *e = "700 + 12 -2 + *($ebp)*19 ";
   expr(e, 0);
   return 0;
 }
