f30ea1645510ace76bac270f3ca2c9d931c8d727
tracer-ics2020 <tracer@njuics.org>
2020-09-11 20:34:41 +0800

    >  compile 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: d806712f879e4edfbbe713b1e8c25455   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-47-generic       Architecture: x86-64  20:34:41 up 1 day, 30 min,  1 user,  load average: 0.29, 0.50, 0.60

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -207,7 +207,7 @@ word_t eval(word_t p, word_t q){
     }
     else if(tokens[p].type==TK_REG){
       bool valid_reg = true;
-      vaddr_t pos = isa_reg_str2val(tokens[p].str, &valid_reg);
+      vaddr_t pos = isa_reg_str2val(tokens[p+1].str, &valid_reg);
       if(valid_reg)return pos;
       else assert(0);
     }
