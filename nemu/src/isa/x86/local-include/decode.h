38fff9cfe3a18cb874a324e64cd3ef7a9bfb100c
tracer-ics2020 <tracer@njuics.org>
2020-09-20 19:28:45 +0800

    >  compile 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: 5677d66a2be34665a1c7c693736c43cc   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-47-generic       Architecture: x86-64  19:28:45 up  5:40,  1 user,  load average: 0.88, 0.48, 0.42

--- nemu/src/isa/x86/local-include/decode.h
+++ nemu/src/isa/x86/local-include/decode.h
@@ -203,7 +203,7 @@ static inline def_DHelper(SI2E) {
   if (id_dest->width == 2) {
     *dsrc1 &= 0xffff;
   }
-  printf("iddestval : %x\n",id_dest->val);
+  printf("iddestval : %x\n",id_src1->val);
 }
 
 static inline def_DHelper(SI_E2G) {
