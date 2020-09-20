1ba59f980dc3b7bb966e88ed67836d214224e256
tracer-ics2020 <tracer@njuics.org>
2020-09-20 19:54:13 +0800

    >  compile 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: 5677d66a2be34665a1c7c693736c43cc   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-47-generic       Architecture: x86-64  19:54:13 up  6:06,  1 user,  load average: 0.33, 0.34, 0.38

--- nemu/src/isa/x86/exec/control.h
+++ nemu/src/isa/x86/exec/control.h
@@ -24,6 +24,7 @@ static inline def_EHelper(jmp_rm) {
 
 static inline def_EHelper(call) {
   // the target address is calculated at the decode stage
+  printf("pcseq:%x\n",s->seq_pc);
   rtl_push(s, &s->seq_pc);
   rtl_j(s,s->jmp_pc);
   print_asm("call %x", s->jmp_pc);
@@ -33,7 +34,6 @@ static inline def_EHelper(ret) {
   s->is_jmp = 1;
   rtl_pop(s, &s->jmp_pc);
   rtl_j(s, s->jmp_pc);
-  printf("jmp:%x\n\n",s->jmp_pc);
   print_asm("ret");
 }
 
