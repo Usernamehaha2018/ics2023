60000e928c62a7213caa6cf1bc89011a8978c519
tracer-ics2020 <tracer@njuics.org>
2020-09-21 15:08:57 +0800

    >  compile 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: 172314a6d8cf424f96d748eb7636750d   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-47-generic       Architecture: x86-64  15:08:57 up 17:31,  1 user,  load average: 1.29, 1.04, 0.91

--- nemu/src/isa/x86/exec/cc.h
+++ nemu/src/isa/x86/exec/cc.h
@@ -29,8 +29,8 @@ static inline void rtl_setcc(DecodeExecState *s, rtlreg_t* dest, uint32_t subcod
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
-    case CC_O:
-    case CC_B:
+    case CC_O: rtl_get_OF(s,dest);break;
+    case CC_B: rtl_get_CF(s,dest);break;
     case CC_E: rtl_get_ZF(s,dest);break;
     case CC_BE:
     case CC_S:
