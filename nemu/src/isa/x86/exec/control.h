6a58caab8598ce7cf9c56e036fb575c9f4e49bcf
tracer-ics2020 <tracer@njuics.org>
2020-09-23 22:19:09 +0800

    >  compile 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: 0746e70a53924b43996d5db51375a0b6   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-47-generic       Architecture: x86-64  22:19:09 up  3:57,  1 user,  load average: 0.37, 0.46, 0.44

--- nemu/src/isa/x86/exec/control.h
+++ nemu/src/isa/x86/exec/control.h
@@ -42,7 +42,8 @@ static inline def_EHelper(ret_imm) {
 }
 
 static inline def_EHelper(call_rm) {
-  TODO();
-
+  rtl_push(s, &s->seq_pc);
+  printf("%x,%x\n",id_dest->imm,s->jmp_pc);
+  rtl_j(s,s->jmp_pc);
   print_asm("call *%s", id_dest->str);
 }
