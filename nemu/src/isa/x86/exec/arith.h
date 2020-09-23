f95e8106729af09a059733987064063f2fe58458
tracer-ics2020 <tracer@njuics.org>
2020-09-23 14:28:47 +0800

    >  compile 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: e88f8f1eaef041ca95564dc897882b41   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-47-generic       Architecture: x86-64  14:28:47 up  4:11,  1 user,  load average: 1.56, 1.28, 1.08

--- nemu/src/isa/x86/exec/arith.h
+++ nemu/src/isa/x86/exec/arith.h
@@ -37,6 +37,7 @@ static inline def_EHelper(sub) {
   rtl_set_CF(s, s1);
   rtl_is_sub_overflow(s, s1, s0, ddest, dsrc1, id_dest->width);
   rtl_set_OF(s, s1);
+  print_asm_template2(sub);
 }
 
 static inline def_EHelper(cmp) {
@@ -54,9 +55,8 @@ static inline def_EHelper(cmp) {
 }
 
 static inline def_EHelper(inc) {
-  id_src1->val = 1;
-  dsrc1 = &id_src1->val;
-  rtl_add(s, s0, ddest, dsrc1);
+  *s1 = 1;
+  rtl_add(s, s0, ddest, s1);
   if (id_dest->width != 4) {
     rtl_andi(s, s0, s0, 0xffffffffu >> ((4 - id_dest->width) * 8));
   }
@@ -70,9 +70,8 @@ static inline def_EHelper(inc) {
 }
 
 static inline def_EHelper(dec) {
-  id_src1->val = 1;
-  dsrc1 = &id_src1->val;
-  rtl_sub(s, s0, ddest, dsrc1);
+  *s1 = 1;
+  rtl_sub(s, s0, ddest, s1);
   if (id_dest->width != 4) {
     rtl_andi(s, s0, s0, 0xffffffffu >> ((4 - id_dest->width) * 8));
   }
