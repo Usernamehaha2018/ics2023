8f5b5e829f8b9226b95a86988bbd987a499aa032
tracer-ics2020 <tracer@njuics.org>
2020-09-23 22:04:39 +0800

    >  compile 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: 0746e70a53924b43996d5db51375a0b6   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-47-generic       Architecture: x86-64  22:04:39 up  3:42,  1 user,  load average: 0.27, 0.35, 0.39

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -69,6 +69,7 @@ static inline def_EHelper(2byte_esc) {
     IDEXW(0x94, setcc_E, setcc, 1)
     IDEXW(0x95, setcc_E, setcc, 1)
     IDEX (0xaf, E2G, imul2)
+    IDEXW(0xbe, E2G, movsx, 1)
     IDEXW(0xbf, E2G, movsx, 2)
     IDEXW (0xb6, mov_E2G, movzx, 1)
     default: exec_inv(s);
