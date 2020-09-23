fc87e5e9a8c48d208afadb20b7e210655ac3aaa9
tracer-ics2020 <tracer@njuics.org>
2020-09-23 19:02:59 +0800

    >  compile 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: 0746e70a53924b43996d5db51375a0b6   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-47-generic       Architecture: x86-64  19:02:59 up 41 min,  1 user,  load average: 0.72, 0.52, 0.51

--- nemu/src/isa/x86/difftest/dut.c
+++ nemu/src/isa/x86/difftest/dut.c
@@ -7,14 +7,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   if(cpu.eax == ref_r->eax&&cpu.ebx == ref_r->ebx
   &&cpu.ecx == ref_r->ecx&&cpu.edx == ref_r->edx
   &&cpu.esp == ref_r->esp&&cpu.ebp == ref_r->ebp
-  &&cpu.esi == ref_r->esi&&cpu.edi == ref_r->edi/*&&cpu.pc == ref_r->pc*/){  
-    if(cpu.pc==0x10009f){
-      printf("reg:\n");
-  for(int i=0;i<8;i++){
-    printf("%d:%x\n",i,ref_r->gpr[i]._32); 
-  }
-  printf("pc:%x\n",ref_r->pc);
-    }
+  &&cpu.esi == ref_r->esi&&cpu.edi == ref_r->edi&&cpu.pc == ref_r->pc){  
     return true;
   }
   printf("reg:\n");
