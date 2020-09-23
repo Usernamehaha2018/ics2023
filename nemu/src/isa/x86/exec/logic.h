7a4bbc27f885fa650fce0f43c6cf79b2adbf6ec5
tracer-ics2020 <tracer@njuics.org>
2020-09-23 18:27:17 +0800

    >  compile 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: 0746e70a53924b43996d5db51375a0b6   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-47-generic       Architecture: x86-64  18:27:17 up 5 min,  1 user,  load average: 0.84, 1.02, 0.56

--- nemu/src/isa/x86/exec/logic.h
+++ nemu/src/isa/x86/exec/logic.h
@@ -27,7 +27,7 @@ static inline def_EHelper(xor) {
   *s1 = 0;
   rtl_set_CF(s,s1);
   rtl_set_OF(s,s1);
-  print_asm_template2(or);
+  print_asm_template2(xor);
 }
 
 static inline def_EHelper(or) {
