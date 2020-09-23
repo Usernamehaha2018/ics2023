f8ada023c753d3845a33574ebc230b1826c3cfbb
tracer-ics2020 <tracer@njuics.org>
2020-09-23 22:57:11 +0800

    >  compile 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: 0746e70a53924b43996d5db51375a0b6   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-47-generic       Architecture: x86-64  22:57:11 up  4:35,  1 user,  load average: 0.86, 0.57, 0.54

--- nemu/src/isa/x86/exec/arith.h
+++ nemu/src/isa/x86/exec/arith.h
@@ -113,11 +113,13 @@ static inline def_EHelper(sbb) {
   rtl_update_ZFSF(s, s1, id_dest->width);
   rtl_is_sub_overflow(s, s2, s1, ddest, dsrc1, id_dest->width);
   rtl_set_OF(s, s2);
+    if (id_dest->width != 4) {
+    rtl_andi(s, s1, s1, 0xffffffffu >> ((4 - id_dest->width) * 8));
+  }
   rtl_is_add_carry(s, s2, s0, dsrc1);
   rtl_is_sub_carry(s, s0, ddest, s0);
   rtl_or(s, s0, s0, s2);
   rtl_set_CF(s, s0);
-  printf("sbb:%u\n",*s1);
   operand_write(s, id_dest, s1);
   print_asm_template2(sbb);
 }
