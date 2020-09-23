b0be7f010a3d67eedad7a58f49943d70e6a5a8d7
tracer-ics2020 <tracer@njuics.org>
2020-09-23 22:46:14 +0800

    >  compile 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: 0746e70a53924b43996d5db51375a0b6   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-47-generic       Architecture: x86-64  22:46:14 up  4:24,  1 user,  load average: 0.77, 0.63, 0.54

--- nemu/src/isa/x86/local-include/rtl.h
+++ nemu/src/isa/x86/local-include/rtl.h
@@ -58,6 +58,7 @@ static inline def_rtl(is_sub_carry, rtlreg_t* dest,
   printf("sub:%u,%u\n",*src1,*src2);
   if(*src1 > *src2) *dest = 1;
   else *dest = 0;
+  printf("%u\n",*dest);
 }
 
 static inline def_rtl(is_add_overflow, rtlreg_t* dest,
