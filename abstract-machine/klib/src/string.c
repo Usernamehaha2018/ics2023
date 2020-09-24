3fa4fc43043b8292bf9d7a654be1b4bcf4ea3ec0
tracer-ics2020 <tracer@njuics.org>
2020-09-24 16:50:41 +0800

    >  compile 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: 0746e70a53924b43996d5db51375a0b6   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-47-generic       Architecture: x86-64  16:50:41 up 22:28,  1 user,  load average: 0.68, 0.63, 0.68

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -56,8 +56,7 @@ int strcmp(const char* s1, const char* s2) {
         ++s1;
         ++s2;
     }
-    printf("ans:%d\n",*s1-*s2);
-    if(*s1 - *s2>0)return 1;
+    if(*s1 - *s2 > 0)return 1;
 
     else return -1;
 }
--- nemu/src/isa/x86/exec/arith.h
+++ nemu/src/isa/x86/exec/arith.h
@@ -85,12 +85,19 @@ static inline def_EHelper(dec) {
 }
 
 static inline def_EHelper(neg) {
-  rtl_neg(s,s0,dsrc1);
+  *s2 = *ddest;
+  rtl_neg(s,s0,dsrc1); 
+   if (id_dest->width != 4) {
+    rtl_andi(s, s0, s0, 0xffffffffu >> ((4 - id_dest->width) * 8));
+  }
+  operand_write(s,id_dest,s0);
   *s1 = 0;
   if(*ddest!=0)*s1 = 1;
   rtl_set_CF(s,s1);
   rtl_update_ZFSF(s,s0, id_dest->width);
-  operand_write(s,id_dest,s0);
+  *s1 = 0;
+  rtl_is_sub_overflow(s,s1, ddest, s1, s2, id_dest->width);
+  rtl_set_OF(s, s1);
   print_asm_template1(neg);
 }
 
