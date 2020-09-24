4b9762e821766e1dbc8805de6e5f092344444750
tracer-ics2020 <tracer@njuics.org>
2020-09-24 15:48:53 +0800

    >  run 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: 0746e70a53924b43996d5db51375a0b6   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-47-generic       Architecture: x86-64  15:48:53 up 21:27,  1 user,  load average: 0.55, 0.79, 0.78

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -52,11 +52,12 @@ int strcmp(const char* s1, const char* s2) {
     while(*s1 == *s2)
     {
         if(*s1 == '\0')
-            return 0;       
+            {return 0; }      
         ++s1;
         ++s2;
     }
-    return *s1 - *s2;
+    if(*s1 - *s2)return 1;
+    else return -1;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
