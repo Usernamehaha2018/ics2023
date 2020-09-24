1d1bffa974e5920a516c7b7432cc7a38f5c82a10
tracer-ics2020 <tracer@njuics.org>
2020-09-24 10:16:57 +0800

    >  compile 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: 0746e70a53924b43996d5db51375a0b6   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-47-generic       Architecture: x86-64  10:16:57 up 15:55,  1 user,  load average: 0.88, 0.77, 0.64

--- nemu/src/isa/x86/exec/arith.h
+++ nemu/src/isa/x86/exec/arith.h
@@ -8,9 +8,9 @@ static inline def_EHelper(add) {
   rtl_update_ZFSF(s, s0, id_dest->width);
   rtl_is_add_carry(s, s1, ddest, dsrc1);
   rtl_set_CF(s, s1);
+  operand_write(s,id_dest,s0);
   rtl_is_add_overflow(s, s1, s0, ddest, dsrc1, id_dest->width);
   rtl_set_OF(s, s1);
-  operand_write(s,id_dest,s0);
   print_asm_template2(add);
 }
 
