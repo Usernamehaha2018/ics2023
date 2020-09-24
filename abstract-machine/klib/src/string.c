d2dc5cb08c7c60f519c2f2d44801e17aca873cc7
tracer-ics2020 <tracer@njuics.org>
2020-09-24 12:08:14 +0800

    >  run 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: 0746e70a53924b43996d5db51375a0b6   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-47-generic       Architecture: x86-64  12:08:14 up 17:46,  1 user,  load average: 0.25, 0.45, 0.49

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -3,12 +3,27 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-size_t strlen(const char *s) {
-  return 0;
+size_t strlen(const char *s) {\
+  if(s==NULL){return 0;}
+  unsigned long i = 0;
+	while((*s++) != '\0')
+		++i;
+	return i;
+}
+
+size_t strnlen(const char *s, size_t count)
+{
+    const char *sc;
+    for (sc = s; count-- && *sc != '\0'; ++sc);
+    return sc - s;
 }
 
 char *strcpy(char* dst,const char* src) {
-  return NULL;
+  if(dst==NULL||src==NULL)
+        return NULL;
+  char *address = dst;  
+	while((*dst++ = *src++) != '\0');  
+	return address;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
@@ -16,7 +31,12 @@ char* strncpy(char* dst, const char* src, size_t n) {
 }
 
 char* strcat(char* dst, const char* src) {
-  return NULL;
+  char *tmp = dst;
+
+    while (*dst)
+        dst++;
+    while ((*dst++ = *src++) != '\0');
+    return tmp;
 }
 
 int strcmp(const char* s1, const char* s2) {
@@ -28,7 +48,10 @@ int strncmp(const char* s1, const char* s2, size_t n) {
 }
 
 void* memset(void* v,int c,size_t n) {
-  return NULL;
+  if(v == NULL)return NULL;
+  char* pDest = (char*)v;
+  while (n-->0)*pDest++ = c;
+  return v; 
 }
 
 void* memmove(void* dst,const void* src,size_t n) {
@@ -36,11 +59,24 @@ void* memmove(void* dst,const void* src,size_t n) {
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  return NULL;
+  if(out==NULL||in==NULL)return NULL;
+  char* pDest = (char*)out;
+  const char* pSrc = (const char*)in;
+  while (n-- > 0){
+  *pDest++ = *pSrc++; }
+  return out;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n) {
-  return 0;
+  if(s1==NULL||s2==NULL) assert(0);
+  const char* pSrc1 = (char*)s1;
+  const char* pSrc2 = (char*)s2;
+  while (n-- > 0){
+    if (*pSrc1++ != *pSrc2++){
+    return *pSrc1 < *pSrc2 ? -1 : 1;
+    }
+  }
+  return 0;  
 }
 
 #endif
