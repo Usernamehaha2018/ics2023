51f0a6cf75d25e10bf55120f1ffd93694082b2b3
tracer-ics2020 <tracer@njuics.org>
2020-09-28 22:27:02 +0800

    >  compile 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: bfa86a867cd84afe9b46a591f5631ed2   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  22:27:02 up  4:57,  1 user,  load average: 1.15, 1.04, 0.89

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -113,6 +113,7 @@ again:
     IDEX (0x03, E2G, add)
     IDEX (0x09, G2E, or)
     IDEX (0x13, E2G, adc)
+    IDEX (0x19, G2E, sbb)
     IDEX (0x1b, E2G, sbb)
     IDEXW(0x22, E2G, and, 1)
     IDEX (0x29, G2E, sub)
