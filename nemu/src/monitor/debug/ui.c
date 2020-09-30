1c4e8640c9bdaa02d874fa4c9aa5aedffb7403e6
tracer-ics2020 <tracer@njuics.org>
2020-09-30 15:35:12 +0800

    >  compile 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: f69eda4a5d9242eb901272432172b898   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  15:35:12 up 1 day, 16:00,  1 user,  load average: 1.56, 1.49, 1.42

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -149,6 +149,7 @@ static int cmd_p(char *args){
       FILE *fp;
        char line[1024];
        fp = fopen("input", "r");
+       printf(">>>\n");
        while (!feof(fp))
        {
                readline(line);
