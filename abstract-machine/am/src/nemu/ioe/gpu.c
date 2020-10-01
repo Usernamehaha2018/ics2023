eac10e37423fdb8c170c30512dfaf6a277b40428
tracer-ics2020 <tracer@njuics.org>
2020-10-02 01:18:50 +0800

    >  compile 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: f69eda4a5d9242eb901272432172b898   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  01:18:50 up 3 days,  1:44,  1 user,  load average: 2.12, 1.65, 1.33

--- abstract-machine/am/src/nemu/ioe/gpu.c
+++ abstract-machine/am/src/nemu/ioe/gpu.c
@@ -1,10 +1,11 @@
 #include <am.h>
 #include <nemu.h>
+#include <string.h>
 
 #define SYNC_ADDR (SCREEN_ADDR + 4)
 
 void __am_gpu_init() {
-   int i;
+  int i;
   int w = 0;  // TODO: get the correct width
   int h = 0;  // TODO: get the correct height
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
@@ -15,12 +16,20 @@ void __am_gpu_init() {
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
+    .width = 400, .height = 300,
     .vmemsz = 0
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+    int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+    uint32_t *pixels = ctl->pixels;
+    uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+    int cp_bytes = sizeof(uint32_t) * (w < (400-x)? w: 400-x);
+    for (int j = 0; j < h && y + j < 300; j ++) {
+        memcpy(&fb[(y + j) * 400 + x], pixels, cp_bytes);
+        pixels += w;
+    }
   if (ctl->sync) {
     outl(SYNC_ADDR, 0);
   }
