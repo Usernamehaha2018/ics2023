750a51d3d85af66c282f01fa62d1cf15a03a779f
tracer-ics2020 <tracer@njuics.org>
2020-10-01 15:59:40 +0800

    >  run 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: f69eda4a5d9242eb901272432172b898   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  15:59:40 up 2 days, 16:25,  1 user,  load average: 1.04, 1.26, 1.26

--- abstract-machine/am/src/nemu/ioe/timer.c
+++ abstract-machine/am/src/nemu/ioe/timer.c
@@ -1,17 +1,16 @@
 #include <am.h>
 #include <nemu.h>
-static uint32_t high, low;
+static uint32_t sec;
 
 
 void __am_timer_init() {
-  low = inl(0x48);
-  high = inl(0x4c);
+  sec = inl(0x4c);
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) { 
   uint32_t lo = inl(0x48);
-  uint32_t hi = inl(0x4c) - high;
-  uptime->us = ((uint64_t)hi << 32) | lo;
+  uint32_t hi = inl(0x4c) - sec;
+  uptime->us = hi * 1000000 + lo;
   // if(new_time < uptime->us%1000000){
   //   uptime->us = new_time + (1000000-uptime->us%1000000)+ uptime->us;
   // }
