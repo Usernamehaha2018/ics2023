0a1b62ace373581ce592d9972939400abcd5b4a0
tracer-ics2020 <tracer@njuics.org>
2020-10-02 13:59:02 +0800

    >  compile 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: f69eda4a5d9242eb901272432172b898   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  13:59:02 up 3 days, 14:24,  1 user,  load average: 1.07, 0.91, 0.89

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -21,7 +21,13 @@ static inline def_EHelper(gp1) {
 
 /* 0xc0, 0xc1, 0xd0, 0xd1, 0xd2, 0xd3 */
 static inline def_EHelper(gp2) {
-  if(s->opcode)
+  if(s->opcode==0xd0){
+     switch (s->isa.ext_opcode) {
+    EX(0,rol) EMPTY(1) EMPTY(2) EMPTY(3)
+    EX(0x4,shl) IDEXW(0x5,empt,shr,1) EMPTY(6) EX(0x7, sar)
+  }
+  return; 
+  }
   switch (s->isa.ext_opcode) {
     EX(0,rol) EMPTY(1) EMPTY(2) EMPTY(3)
     EX(0x4,shl) EX(0x5,shr) EMPTY(6) EX(0x7, sar)
