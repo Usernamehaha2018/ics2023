25f0ed0af494546b911962fdfe55f04065a3af59
tracer-ics2020 <tracer@njuics.org>
2020-10-02 19:20:58 +0800

    >  run 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: f69eda4a5d9242eb901272432172b898   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  19:20:58 up 3 days, 19:46,  1 user,  load average: 1.30, 1.14, 1.00

--- abstract-machine/am/src/nemu/ioe/input.c
+++ abstract-machine/am/src/nemu/ioe/input.c
@@ -8,8 +8,8 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
     int code_cur = inl(0x60) & 0xff;
     code <<= 24;
     code >>= 24;
-  if(code){
-    kbd->keydown = (code_cur & KEYDOWN_MASK) < 128;
+  if(code & KEYDOWN_MASK){
+    kbd->keydown = code_cur < 128;
     kbd->keycode = code;
   }
   else{
