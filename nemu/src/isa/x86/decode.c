a83fae1fccce15dc14ecb7c3ed867257f44e8001
tracer-ics2020 <tracer@njuics.org>
2020-10-04 00:12:17 +0800

    >  compile 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: f69eda4a5d9242eb901272432172b898   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  00:12:17 up 5 days, 37 min,  1 user,  load average: 0.81, 0.77, 0.80

--- nemu/src/isa/x86/decode.c
+++ nemu/src/isa/x86/decode.c
@@ -101,6 +101,8 @@ void read_ModR_M(DecodeExecState *s, Operand *rm, bool load_rm_val, Operand *reg
   ModR_M m;
   m.val = instr_fetch(&s->seq_pc, 1);
   s->isa.ext_opcode = m.opcode;
+  printf("%d\n",reg==NULL);
+  printf("%d\n",m.mod==3);
   if (reg != NULL) operand_reg(s, reg, load_reg_val, m.reg, reg->width);
   if (m.mod == 3) operand_reg(s, rm, load_rm_val, m.R_M, rm->width);
   else {
