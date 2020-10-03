3a524436904246ef7b609113995fc390dabde1f6
tracer-ics2020 <tracer@njuics.org>
2020-10-04 00:32:09 +0800

    >  compile 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: f69eda4a5d9242eb901272432172b898   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  00:32:09 up 5 days, 57 min,  1 user,  load average: 1.17, 1.07, 1.00

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -97,7 +97,7 @@ static inline void fetch_decode_exec(DecodeExecState *s) {
 again:
   opcode = instr_fetch(&s->seq_pc, 1); // 取指
   s->opcode = opcode;  //操作码
-  // printf("pc:%x\n",cpu.pc);
+  printf("%x\n",cpu.pc);
 //   if (opcode == 0xd3) printf("op:%x,pc:%x\n",opcode,cpu.pc);
 // if(opcode==0xd3){
 //     isa_reg_display();
