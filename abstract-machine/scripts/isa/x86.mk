b901fa4527fce430c3bf14a3ecef874613c920bd
tracer-ics2020 <tracer@njuics.org>
2020-10-11 14:24:57 +0800

    >  run 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: df7f781c63b949eaa4ab35c3dfc88be2   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  14:24:57 up 4 days, 21:16,  1 user,  load average: 0.59, 0.75, 0.66

--- abstract-machine/scripts/isa/x86.mk
+++ abstract-machine/scripts/isa/x86.mk
@@ -1,4 +1,4 @@
 CFLAGS  += -m32 -fno-pic -fno-omit-frame-pointer -march=i386
--fcf-protection=branch -mmanual-endbr
+-fcf-protection=none -mmanual-endbr
 ASFLAGS += -m32 -fno-pic
 LDFLAGS += -melf_i386
