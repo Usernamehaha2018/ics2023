1175649bff2c064bdcfee4d278862149e473aa59
tracer-ics2020 <tracer@njuics.org>
2020-10-20 07:32:03 +0800

    >  run 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: a43bb3f092bc45778ac5bd23d7b547ac   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  07:32:03 up 2 days,  8:30,  1 user,  load average: 0.96, 1.45, 1.22

--- abstract-machine/am/src/nemu/isa/x86/cte.c
+++ abstract-machine/am/src/nemu/isa/x86/cte.c
@@ -17,7 +17,7 @@ void __am_vecnull();
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
-    printf("haha,%d",c->irq);
+    printf("haha,%d",c->eax);
     switch (c->irq) {
       default: ev.event = EVENT_ERROR; break;
     }
