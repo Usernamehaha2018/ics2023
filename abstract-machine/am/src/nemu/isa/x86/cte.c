421d688231da1fca9a8f22549e5d6333f48a0c01
tracer-ics2020 <tracer@njuics.org>
2020-10-20 21:14:02 +0800

    >  compile 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: a43bb3f092bc45778ac5bd23d7b547ac   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  21:14:02 up 2 days, 22:12,  1 user,  load average: 1.30, 1.27, 1.27

--- abstract-machine/am/src/nemu/isa/x86/cte.c
+++ abstract-machine/am/src/nemu/isa/x86/cte.c
@@ -17,8 +17,29 @@ void __am_vecnull();
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
+// eax     53      83
+// ecx     132fd7  1257431
+// edx     3f8     1016
+// ebx     133f70  1261424
+// esp     133f50  1261392
+// ebp     133f68  1261416
+// esi     4c066120        1275486496
+// edi     28ed23cc        686629836
+    printf("%d\n",c->irq);
+    printf("%d\n",c->ess);
+    printf("%d\n",c->eax);
+    printf("%d\n",c->ecx);
+    printf("%d\n",c->edx);
+    printf("%d\n",c->ebx);
+    printf("%d\n",c->esp);
+    printf("%d\n",c->ebp);
+    printf("%d\n",c->esi);
+    printf("%d\n",c->ebp);
+    printf("%d\n",c->eflags);
+    printf("%d\n",c->cs);
+    printf("%d\n",c->eip);
     switch (c->irq) {
-      case 0x81: ev.event = EVENT_YIELD;break;
+      case 0x81: ev.event = EVENT_YIELD; break;
       default: ev.event = EVENT_ERROR; break;
     }
     c = user_handler(ev, c);
