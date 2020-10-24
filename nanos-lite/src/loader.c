f7f2dd38cb153e2704b11f7b04819c5e5225cea0
tracer-ics2020 <tracer@njuics.org>
2020-10-24 22:28:06 +0800

    >  run 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: 3b67af6ea9a945d0aae0e010dfe5d6ca   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-51-generic       Architecture: x86-64  22:28:06 up 3 days,  3:20,  1 user,  load average: 0.60, 0.62, 0.88

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -15,14 +15,11 @@ size_t get_ramdisk_size();
 
 static uintptr_t loader(PCB *pcb, const char *filename) {  
   Elf_Ehdr ret_buf;
-  void* cur;
   ramdisk_read(&ret_buf, 0, sizeof(Elf_Ehdr));
   Elf_Phdr phdr[ret_buf.e_phnum];
   for(int i = 0; i<ret_buf.e_phnum;i++){
     ramdisk_read(&phdr[i], ret_buf.e_phoff+i*sizeof(Elf_Phdr), sizeof(Elf_Phdr));
     ramdisk_read((void*)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_memsz);
-    cur = (void*)(phdr[i].p_filesz+phdr[i].p_offset);
-    memset(cur, 0, phdr[i].p_memsz - phdr[i].p_filesz);
   }
   
   return ret_buf.e_entry;
