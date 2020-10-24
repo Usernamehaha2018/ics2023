3b9aaea83a0910724e562dac76be9123093d9afe
tracer-ics2020 <tracer@njuics.org>
2020-10-24 21:31:10 +0800

    >  run 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: 3b67af6ea9a945d0aae0e010dfe5d6ca   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-51-generic       Architecture: x86-64  21:31:10 up 3 days,  2:23,  1 user,  load average: 2.11, 1.41, 1.17

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -13,28 +13,15 @@ size_t ramdisk_read(void *buf, size_t offset, size_t len);
 
 size_t get_ramdisk_size();
 
-static uintptr_t loader(PCB *pcb, const char *filename) {
-  Elf32_Ehdr buf;
-  ramdisk_read(&buf, 0, sizeof(Elf_Ehdr));
-  printf("%u,\n",buf.e_phnum);
-  Elf_Phdr phdr[buf.e_phnum];
-  for(int i = 0; i<buf.e_phnum;i++){
-    ramdisk_read(&phdr[i], buf.e_phoff+i*sizeof(Elf_Phdr), sizeof(Elf_Phdr));
-  }
-  for(int i = 0; i<buf.e_phnum;i++){
-    printf("eeiche,\n");
-    printf("%u,\n",phdr[i].p_offset);
-  }
-  void* buf1 =  (void *)0x3000000;
-  void* buf2 =  (void *)0x3001000;
-  void* buf3 =  (void *)0x3005000;
-  void* buf4 =  (void *)0x30062a0;
+static uintptr_t loader(PCB *pcb, const char *filename) {  
   Elf_Ehdr ret_buf;
   ramdisk_read(&ret_buf, 0, sizeof(Elf_Ehdr));
-  ramdisk_read(buf1, 0, 0xd4);
-  ramdisk_read(buf2, 0x1000, 0x3b93);
-  ramdisk_read(buf3, 0x5000, 0x28c);
-  ramdisk_read(buf4, 0x52a0, 0x918);
+  Elf_Phdr phdr[ret_buf.e_phnum];
+  for(int i = 0; i<ret_buf.e_phnum;i++){
+    ramdisk_read(&phdr[i], ret_buf.e_phoff+i*sizeof(Elf_Phdr), sizeof(Elf_Phdr));
+    ramdisk_read((void*)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_memsz);
+  }
+
   return ret_buf.e_entry;
 }
 
