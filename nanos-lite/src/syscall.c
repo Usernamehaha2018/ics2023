302595d5d63baa72f5deaba9b580483a93cb51f7
tracer-ics2020 <tracer@njuics.org>
2020-10-24 17:58:14 +0800

    >  compile 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: 3b67af6ea9a945d0aae0e010dfe5d6ca   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-51-generic       Architecture: x86-64  17:58:14 up 2 days, 22:50,  1 user,  load average: 0.74, 0.63, 0.62

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -10,7 +10,6 @@ void sys_yield(Context *c) {
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-  printf("debug:%u \n", a[0]);
   switch (a[0]) {
     case 1: sys_yield(c);break; //SYS_yield
     default: panic("Unhandled syscall ID = %d", a[0]);
--- nemu/src/isa/x86/difftest/dut.c
+++ nemu/src/isa/x86/difftest/dut.c
@@ -9,6 +9,14 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   &&cpu.esp == ref_r->esp&&cpu.ebp == ref_r->ebp
   &&cpu.esi == ref_r->esi&&cpu.edi == ref_r->edi/*&&cpu.pc == ref_r->pc*/){  
     //printf("pc:%x,%x\n",cpu.pc,ref_r->pc);
+    if(cpu.pc==0x0300105b||cpu.pc==0x0300105c)
+    {
+        printf("reg:\n");
+  for(int i=0;i<8;i++){
+    printf("%d:%x\n",i,ref_r->gpr[i]._32); 
+  }
+  printf("pc:%x\n",ref_r->pc);
+    }
     return true;
   }
   printf("reg:\n");
