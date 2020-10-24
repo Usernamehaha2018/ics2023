c37b59813842c3e56b80d24d5592c0fa68b8229f
tracer-ics2020 <tracer@njuics.org>
2020-10-24 19:43:40 +0800

    >  compile 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: 3b67af6ea9a945d0aae0e010dfe5d6ca   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-51-generic       Architecture: x86-64  19:43:40 up 3 days, 36 min,  1 user,  load average: 0.91, 0.96, 0.82

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -16,15 +16,8 @@ size_t get_ramdisk_size();
 static uintptr_t loader(PCB *pcb, const char *filename) {
   void* buf =  (void *)0x3000000;
   Elf_Ehdr ret_buf;
-  Elf_Phdr p_buf;
   ramdisk_read(&ret_buf, 0, sizeof(Elf_Ehdr));
-  ramdisk_read(&p_buf, 0, sizeof(Elf_Phdr));
-  printf("%u,\n",p_buf.p_offset);
-  printf("%u,\n",p_buf.p_vaddr);
-  printf("%u,\n",p_buf.p_paddr);
-  printf("%u,\n",p_buf.p_filesz);
-  printf("%u,\n",p_buf.p_memsz);
-printf("\n\n");
+  printf("%u\n",ret_buf.e_phnum);
   size_t  len = get_ramdisk_size();
   ramdisk_read(buf, 0, len);
   return ret_buf.e_entry;
--- nemu/src/isa/x86/difftest/dut.c
+++ nemu/src/isa/x86/difftest/dut.c
@@ -9,14 +9,6 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   &&cpu.esp == ref_r->esp&&cpu.ebp == ref_r->ebp
   &&cpu.esi == ref_r->esi&&cpu.edi == ref_r->edi/*&&cpu.pc == ref_r->pc*/){  
     //printf("pc:%x,%x\n",cpu.pc,ref_r->pc);
-    if(cpu.pc>0x030012e0&&cpu.pc<=0x030012f4)
-    {
-        printf("reg:\n");
-  for(int i=0;i<8;i++){
-    printf("%d:%x\n",i,ref_r->gpr[i]._32); 
-  }
-  printf("pc:%x\n",ref_r->pc);
-    }
     return true;
   }
   printf("reg:\n");
