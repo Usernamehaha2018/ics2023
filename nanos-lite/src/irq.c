97b5b38a4f86e09593a52a909b2e050d31e1dd90
tracer-ics2020 <tracer@njuics.org>
2020-10-24 16:53:23 +0800

    >  compile 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: 3b67af6ea9a945d0aae0e010dfe5d6ca   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-51-generic       Architecture: x86-64  16:53:23 up 2 days, 21:45,  1 user,  load average: 0.56, 0.68, 0.82

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,7 +3,7 @@ void do_syscall(Context *c);
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_YIELD:printf("Handled event id =1\n");break;
-    case EVENT_SYSCALL:  printf("2eax:%u\n",c->eax);do_syscall(c);break;
+    case EVENT_SYSCALL:  do_syscall(c);break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -10,9 +10,6 @@ void sys_yield(Context *c) {
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-  a[1] = c->GPR2;
-  a[2] = c->GPR3;
-  a[3] = c->GPR4;
   switch (a[0]) {
     case 1: sys_yield(c);break; //SYS_yield
     default: panic("Unhandled syscall ID = %d", a[0]);
