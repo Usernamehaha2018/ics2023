f090f3ec48ebad46afc81a3aca68cc98a3c7d9c5
tracer-ics2020 <tracer@njuics.org>
2020-10-24 17:31:30 +0800

    >  run 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: 3b67af6ea9a945d0aae0e010dfe5d6ca   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-51-generic       Architecture: x86-64  17:31:30 up 2 days, 22:23,  1 user,  load average: 1.09, 0.77, 0.75

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,7 +2,7 @@
 void do_syscall(Context *c);
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    case EVENT_YIELD:printf("Handled event id =1\n");break;
+    case EVENT_YIELD:break;
     case EVENT_SYSCALL:  do_syscall(c);break;
     default: panic("Unhandled event ID = %d", e.event);
   }
