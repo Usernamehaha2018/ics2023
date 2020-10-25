9bf780712b3c586ac0bd04b3a2ebc5f4f4b32346
tracer-ics2020 <tracer@njuics.org>
2020-10-26 00:22:55 +0800

    >  run 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: 62a4ae3cfdb54c65a792bfa19cf9e7ba   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  00:22:55 up 1 day,  1:26,  1 user,  load average: 1.60, 1.25, 1.05

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -7,6 +7,7 @@
 
 int _gettimeofday(struct timeval *tv, struct timezone *tz);
 int _signal(void *buf, size_t len);
+int _get_screen_size(void *buf, size_t len);
 static int evtdev = -1;
 static int fbdev = -1;
 static int screen_w = 0, screen_h = 0;
@@ -29,15 +30,48 @@ int NDL_PollEvent(char *buf, int len) {
   return _signal(buf,len);
 }
 
+void get_width(char* buf, int* width, int* height){
+  int ans_w=0, ans_h = 0;
+  int flag = 0;
+  while(*buf != '\n'){
+    if(*buf > '0' && *buf < '9'){
+      ans_w = ans_w *10 + (*buf - '0');
+    }
+    buf ++;
+  }
+  buf ++;
+  while(*buf != '\n'){
+    if(*buf > '0' && *buf < '9'){
+      ans_h = ans_h *10 + (*buf - '0');
+    }
+    buf ++;
+  }
+  *width = ans_w;
+  *height = ans_h;
+  return;
+}
+
 void NDL_OpenCanvas(int *w, int *h) {
   if (getenv("NWM_APP")) {
     int fbctl = 4;
     fbdev = 5;
     screen_w = *w; screen_h = *h;
     char buf[64];
-    int len = sprintf(buf, "%d %d", screen_w, screen_h);
+    _get_screen_size(buf, sizeof(buf));
+    //int len = sprintf(buf, "%d %d", screen_w, screen_h);
+    char *cur = &buf[0];
+    int s_width=0,s_height = 0;
+    get_width(cur,&s_width,&s_height);
+    if(!*w||!*h){
+      screen_h = s_height;
+      screen_w = s_width;
+    }
+    printf("%d-----%d\n",s_height,s_width);
+    while(1){
+      
+    }
     // let NWM resize the window and create the frame buffer
-    write(fbctl, buf, len);
+    //write(fbctl, buf, len);
     while (1) {
       // 3 = evtdev
       int nread = read(3, buf, sizeof(buf) - 1);
