9fe956798a08d7cbba7a5d279b2bf7fd982f860e
tracer-ics2020 <tracer@njuics.org>
2020-10-25 14:24:42 +0800

    >  run 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: 62a4ae3cfdb54c65a792bfa19cf9e7ba   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  14:24:42 up 15:28,  1 user,  load average: 0.61, 0.56, 0.49

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -29,11 +29,17 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
   return 0;
 }
 
+size_t sys_write_standard(const void *buf, size_t offset, size_t len) {
+  for (int i=0; i<len ;i++)
+      putch(((char *)buf)[i]);
+  return len;
+}
+
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
   [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
-  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
-  [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
+  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, sys_write_standard},
+  [FD_STDERR] = {"stderr", 0, 0, invalid_read, sys_write_standard},
 #include "files.h"
 };
 
