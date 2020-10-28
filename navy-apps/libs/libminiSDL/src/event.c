a65956a0bb4706cfd53e6f675da3585312eced3c
tracer-ics2020 <tracer@njuics.org>
2020-10-28 23:12:28 +0800

    >  run 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: 0fd7683e7c0547029671dce8862155db   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  23:12:28 up 31 min,  1 user,  load average: 1.25, 1.05, 0.88

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -90,6 +90,6 @@ uint8_t* SDL_GetKeyState(int *numkeys) {
     if(i==down)keystate[i]=1;
     else keystate[i]= 0;
   }
-  printf("down:%d;\n",down);
+  // printf("down:%d;\n",down);
   return keystate;
 }
