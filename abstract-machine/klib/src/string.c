beac794b579f8b4dfd36858c635073ef50fdacb4
tracer-ics2020 <tracer@njuics.org>
2020-10-29 15:45:28 +0800

    >  run 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: 0fd7683e7c0547029671dce8862155db   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  15:45:28 up 17:04,  1 user,  load average: 2.12, 1.89, 1.90

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -4,11 +4,12 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {\
-printf("here!\n");
   assert(s != NULL);
 	int len = 0;
 	while((*s++) != '\0')
 		++len;
+  printf("%s\n",s);
+  printf("len%d\n",len);
 	return len;
 }
 
