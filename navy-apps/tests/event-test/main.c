5690183d643b08ebfbc9053526de1ad0f12a3a99
tracer-ics2020 <tracer@njuics.org>
2020-10-29 23:34:52 +0800

    >  run 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: 0fd7683e7c0547029671dce8862155db   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  23:34:52 up 1 day, 53 min,  1 user,  load average: 1.40, 1.45, 1.74

--- navy-apps/tests/event-test/main.c
+++ navy-apps/tests/event-test/main.c
@@ -4,7 +4,11 @@
 int main() {
   NDL_Init(0);
   while (1) {
-
+    char buf[64]="emm";
+    NDL_PollEvent(buf, sizeof(buf));
+    if (1) {
+      printf("receive event: %s\n", buf);
+    }
   }
   return 0;
 }
