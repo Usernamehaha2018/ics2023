67c4c5554286b564fa62c80ef3dfe89cf6c0c7e4
tracer-ics2020 <tracer@njuics.org>
2020-10-29 22:35:00 +0800

    >  run 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: 0fd7683e7c0547029671dce8862155db   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  22:35:00 up 23:53,  1 user,  load average: 2.73, 2.37, 2.06

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -50,9 +50,15 @@ size_t events_get(void *buf) {
   AM_INPUT_KEYBRD_T kbd;
   __am_input_keybrd(&kbd);
   uint8_t* m = buf;
+  int flag = 0;
   for(int i=0;i<256;i++){
     m[i] = ((i==kbd.keycode&&kbd.keydown)? 1: 0);
+    if(m[i]){
+      flag = i;
+    }
   }
+  if(!flag)printf("no key\n");
+  else printf("key:%d\n",flag);
   return 0;
 }
 
