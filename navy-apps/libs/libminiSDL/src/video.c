ea4fb4a47aeb3ebd5f9286c91813c811dc433100
tracer-ics2020 <tracer@njuics.org>
2020-10-30 20:18:24 +0800

    >  run 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: 0fd7683e7c0547029671dce8862155db   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  20:18:24 up 1 day, 21:37,  1 user,  load average: 1.32, 1.19, 1.04

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -98,10 +98,10 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
       for(int i = 0;i<s->w;i++){     
             c = s->pixels[ i + j * s->w];   
             // if(c)printf("%d\n",c);
-            m[count++] = s->format->palette->colors[c].b;
-            m[count++] = s->format->palette->colors[c].r;
             m[count++] = s->format->palette->colors[c].a;
             m[count++] = s->format->palette->colors[c].g;
+            m[count++] = s->format->palette->colors[c].b;
+            m[count++] = s->format->palette->colors[c].r;
       }
     }
     time_spend += (NDL_GetTicks()-time_bef);
