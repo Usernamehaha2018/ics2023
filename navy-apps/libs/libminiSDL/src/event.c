e1e91ebffde7ecb39e379f83d8c7ba3d9a32b5a9
tracer-ics2020 <tracer@njuics.org>
2020-10-30 18:24:17 +0800

    >  run 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: 0fd7683e7c0547029671dce8862155db   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  18:24:17 up 1 day, 19:42,  1 user,  load average: 1.65, 1.47, 1.24

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -122,8 +122,7 @@ uint8_t* SDL_GetKeyState(int *numkeys) {
   //     // if(keystate[i])printf("i:%d\n",i);
   // }
   for (int i = 0; i < 83; i++) keystate[i] = 0;
-  assert(key_f != key_r);
-  for (int i = key_f; i != key_r; i = (i + 1) % KEY_QUEUE_LEN) {
+  for (int i = key_f-1; i != key_r; i = (i + 1) % KEY_QUEUE_LEN) {
     if (key_queue[i].state = 0) keystate[key_queue[i].key] = 1;
     else keystate[key_queue[i].key] = 0;
   }
