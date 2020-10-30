09fa420c44b9b0c4b935f22b44ffae3bd865ef0e
tracer-ics2020 <tracer@njuics.org>
2020-10-30 11:24:50 +0800

    >  run 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: 0fd7683e7c0547029671dce8862155db   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  11:24:50 up 1 day, 12:43,  1 user,  load average: 1.73, 1.90, 1.97

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -12,62 +12,68 @@ static const char *keyname[] = {
   _KEYS(keyname)
 };
 
-int SDL_PushEvent(SDL_Event *ev) {
-  printf("unhandled pushevent\n");
-  return 0;
-}
+#define KEY_QUEUE_LEN 1024
+struct event_state{
+  int key;
+  int state;
+};
+static struct event_state key_queue[KEY_QUEUE_LEN];
+static int key_f = 0, key_r = 0;
 
-int SDL_PollEvent(SDL_Event *ev) {
-  // printf("poll here\n");
+int SDL_PumpEvent() {
   char s[128];
   int flag = 1;
   int ans = NDL_PollEvent(s,128);
   if(ans){
     if(s[1]=='d'){
       printf("%s,s.\n",s);
-      ev->type = 0;
+      key_queue[key_r].state = 0;
     }
-    else if (s[1]=='u')ev->type = 1;
+    else if (s[1]=='u')key_queue[key_r].state = 1;
     int flag  = 0;
     char *kbd = &s[3];    
     for(uint8_t i= 0;i<83;i++){
       if(strncmp(keyname[i],(const char*)kbd,strlen(keyname[i]))==0&&strlen((const char*)kbd)-1==strlen(keyname[i])){
        down = i;
-       ev->key.keysym.sym = i;
+       key_queue[key_r].key = i;
        flag = 1;
       }
     }  
     if(!flag){assert(0);}
+    key_r = (key_r + 1) % KEY_QUEUE_LEN;
+    return 1;
   }
-  else{
-    return ans;
-  }
-  return ans;
+  return 0;
 }
 
-int SDL_WaitEvent(SDL_Event *event) {
-  char s[128];
-  int flag = 1;
-  int ans = 0;
-  while(!ans){
-  ans = NDL_PollEvent(s,128);
-  if(ans){
-    if(s[1]=='d'){
-      event->type = 0;
-    }
-    else if (s[1]=='u')event->type = 1;
-    char *kbd = &s[3];    
-    int flag = 0;
-    for(uint8_t i= 0;i<83;i++){
-      if(strncmp(keyname[i],(const char*)kbd,strlen(keyname[i]))==0&&strlen((const char*)kbd)-1==strlen(keyname[i])){
-       event->key.keysym.sym = i;
-       flag =1;
-      }
-    }  
-    if(!flag)assert(0);
+
+int SDL_PushEvent(SDL_Event *ev) {
+  printf("unhandled pushevent\n");
+  return 0;
+}
+
+int SDL_PollEvent(SDL_Event *ev) {
+  // printf("poll here\n");  
+  SDL_PumpEvent();
+  if(key_f == key_r)return 0;
+  if(ev != NULL){     
+    ev->key.keysym.sym = key_queue[key_f].key;
+    ev->type = key_queue[key_f].state;
+    key_f = (key_f + 1) % KEY_QUEUE_LEN;
+  }    
+  return 1;   
+}
+
+int SDL_WaitEvent(SDL_Event *ev) {
+  while(key_f == key_r){
+    SDL_PumpEvent();
   }
+    if(ev != NULL){     
+    ev->key.keysym.sym = key_queue[key_f].key;
+    ev->type = key_queue[key_f].state;
+    key_f = (key_f + 1) % KEY_QUEUE_LEN;
   }
-  return ans;
+  return 1;   
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
@@ -75,7 +81,16 @@ int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
   return 0;
 }
 
+
 uint8_t* SDL_GetKeyState(int *numkeys) {
-  NDL_get_key(keystate);
-  return keystate;
+  SDL_PumpEvent();  
+  if(key_f!=key_r){
+    for(int i=0;i<256;i++)
+      keystate[i] = (i==key_queue[key_f].key?1:0);    
+  }
+  else{
+    for(int i=0;i<256;i++)
+      keystate[i] = 0;   
+  }
+  return keystate;   
 }
