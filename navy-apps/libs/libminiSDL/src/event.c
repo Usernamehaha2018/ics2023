906a24f728e02ed05e143a70f697946236e761b2
tracer-ics2020 <tracer@njuics.org>
2020-10-30 13:10:46 +0800

    >  run 181860098    Static hostname: computer          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6745292b43d24860976b613ced153c2b            Boot ID: 0fd7683e7c0547029671dce8862155db   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  13:10:46 up 1 day, 14:29,  1 user,  load average: 1.79, 1.71, 1.67

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -85,7 +85,7 @@ int SDL_PollEvent(SDL_Event *ev) {
   if(ev != NULL){     
     ev->key.keysym.sym = key_queue[key_f].key;
     ev->type = key_queue[key_f].state;
-    key_f = (key_f + 1) % KEY_QUEUE_LEN;
+    // key_f = (key_f + 1) % KEY_QUEUE_LEN;
     // printf("eventpollhappen,key_f:%d\n",key_f);
   }
   if(ev==NULL){
@@ -102,7 +102,7 @@ int SDL_WaitEvent(SDL_Event *ev) {
     if(ev != NULL){     
     ev->key.keysym.sym = key_queue[key_f].key;
     ev->type = key_queue[key_f].state;
-    key_f = (key_f + 1) % KEY_QUEUE_LEN;
+    // key_f = (key_f + 1) % KEY_QUEUE_LEN;
     // printf("eventwaithappen,key_f:%d\n",key_f);
   }
   return 1;   
@@ -116,19 +116,20 @@ int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
 
 uint8_t* SDL_GetKeyState(int *numkeys) {
   SDL_PumpEvent();  
-  // printf("key_l&r:%d,%d\n",key_f,key_r);
-  // for(int i=0;i<83;i++) {
-  //   keystate[i] = (i==key_queue[key_f].key?1:0);    
-  //     // if(keystate[i])printf("i:%d\n",i);
-  // }
-    for (int i = key_f; i != key_r; i = (i + 1) % KEY_QUEUE_LEN) {
-      if (key_queue[i].state == 0) {
-        keystate[key_queue[i].key] = 1;
-      }
-      else {
-        keystate[key_queue[i].key] == 0;
-      }
-    }
+  printf("key_l&r:%d,%d\n",key_f,key_r);
+  for(int i=0;i<83;i++) {
+    keystate[i] = (i==key_queue[key_f].key?1:0);    
+      // if(keystate[i])printf("i:%d\n",i);
+  }
+  key_f = (key_f + 1) % KEY_QUEUE_LEN;
+    // for (int i = key_f; i != key_r; i = (i + 1) % KEY_QUEUE_LEN) {
+    //   if (key_queue[i].state == 0) {
+    //     keystate[key_queue[i].key] = 1;
+    //   }
+    //   else {
+    //     keystate[key_queue[i].key] == 0;
+    //   }
+    // }
   
   return keystate;   
 }
